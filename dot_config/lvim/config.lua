8c5f8a6df128214a12555cc43fffb8c83e1739b9
ailac22 <imorenoval@protonmail.com>
2024-03-25 01:48:57 +0100

    Volviendo a firefox derrotado. Quitados algunos dotfiles inútiles

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -172,40 +172,6 @@ lvim.keys.normal_mode["<F6>"] = "i```\n```<ESC><UP>"
 vim.keymap.set("n", "<F10>", ":BufferLineTogglePin<cr>")
 vim.keymap.set("n", "<F9>", ":BufferLineGoToBuffer 1<cr>")
 
-local function set_harpoon_keymaps()
-  lvim.keys.normal_mode["<C-Space>"] = "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>"
-  lvim.keys.normal_mode["tu"] = "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>"
-  lvim.keys.normal_mode["te"] = "<cmd>lua require('harpoon.term').gotoTerminal(2)<CR>"
-  lvim.keys.normal_mode["cu"] = "<cmd>lua require('harpoon.term').sendCommand(1, 1)<CR>"
-  lvim.keys.normal_mode["ce"] = "<cmd>lua require('harpoon.term').sendCommand(1, 2)<CR>"
-  lvim.keys.normal_mode["<C-n>"] = "<CMD>lua require('harpoon.ui').nav_file(8)<CR>"
-  lvim.keys.normal_mode["<leader>v"] = "<CMD>lua require('harpoon.ui').nav_prev()<CR>"
-  lvim.keys.normal_mode["<leader>n"] = "<CMD>lua require('harpoon.ui').nav_next()<CR>"
-  lvim.builtin.which_key.mappings["a"] = { "<cmd>lua require('harpoon.mark').add_file()<CR>", "Add Mark" }
-  lvim.builtin.which_key.mappings["<leader>"] = {
-    "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
-    "Harpoon",
-  }
-
-  local whk_status, whk = pcall(require, "which-key")
-  if not whk_status then
-    return
-  end
-  -- whk.register {
-  --   ["<leader>1"] = { "<CMD>lua require('harpoon.ui').nav_file(1)<CR>", "goto1" },
-  --   ["<leader>2"] = { "<CMD>lua require('harpoon.ui').nav_file(2)<CR>", "goto2" },
-  --   ["<leader>3"] = { "<CMD>lua require('harpoon.ui').nav_file(3)<CR>", "goto3" },
-  --   ["<leader>4"] = { "<CMD>lua require('harpoon.ui').nav_file(4)<CR>", "goto4" },
-  --   ["<leader>5"] = { "<CMD>lua require('harpoon.ui').nav_file(5)<CR>", "goto5" },
-  --   ["<leader>6"] = { "<CMD>lua require('harpoon.ui').nav_file(6)<CR>", "goto6" },
-  --   ["<leader>7"] = { "<CMD>lua require('harpoon.ui').nav_file(7)<CR>", "goto7" },
-  --   ["<leader>8"] = { "<CMD>lua require('harpoon.ui').nav_file(8)<CR>", "goto8" },
-  --   ["<leader>9"] = { "<CMD>lua require('harpoon.ui').nav_file(9)<CR>", "goto9" }
-  -- }
-end
-
-set_harpoon_keymaps()
-
 -- Additional Plugins
 lvim.plugins = {
   { "folke/tokyonight.nvim" },
