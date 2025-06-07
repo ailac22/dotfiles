2d1a3101653fd66f23424b2d903c49c0c83f35fd
ailac22 <imorenoval@protonmail.com>
2025-06-07 10:55:35 +0200

    my-mod in neovim, fuzzel

--- dot_config/nvim/lua/plugins/leap.lua
+++ dot_config/nvim/lua/plugins/leap.lua
@@ -1,3 +1,4 @@
+-- my-mod
 return {
   "ggandor/leap.nvim",
   config = function()
--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,5 @@
+-- my-mod
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
